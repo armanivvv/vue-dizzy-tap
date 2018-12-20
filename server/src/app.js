32854381ad3e7ebb30292763e678f6b4d6679409
Armani <armanivvv@gmail.com>
2018-12-20 19:04:51 +0800

    SAVEPOINT

--- server/src/app.js
+++ server/src/app.js
@@ -15,4 +15,10 @@ app.get('/status', (req, res) => {
   });
 });
 
+app.post('/register', (req, res) => {
+  res.send({
+    message: `User ${req.body.email}! was registered!! Enjoy.`,
+  });
+});
+
 app.listen(process.env.PORT || 8081);
