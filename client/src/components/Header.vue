6ecc2d9e4cefd466d1a1349b5d4fc7db4753f930
Armani <armanivvv@gmail.com>
2018-12-22 08:22:11 +0800

    logout button

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -14,11 +14,20 @@
     <v-spacer></v-spacer>
 
     <v-toolbar-items>
-      <v-btn flat dark v-if="!$store.state.isUserLoggedIn">
-        <router-link :to="{ name: 'login' }">Login</router-link>
+      <v-btn flat dark v-if="!$store.state.isUserLoggedIn"
+      @click="navigateTo({ name: 'login' })">
+        <!-- <router-link :to="{ name: 'login' }">Login</router-link> -->
+        Login
       </v-btn>
-      <v-btn flat dark v-if="!$store.state.isUserLoggedIn">
-        <router-link :to="{ name: 'register' }">Sign Up</router-link>
+      <v-btn flat dark v-if="!$store.state.isUserLoggedIn"
+      @click="navigateTo({ name: 'register' })">
+        <!-- <router-link :to="{ name: 'register' }">Sign Up</router-link> -->
+        Signup
+      </v-btn>
+      <v-btn flat dark v-if="$store.state.isUserLoggedIn"
+      @click="logout">
+        <!-- <router-link :to="{ name: 'logout' }">Logout</router-link> -->
+        Logout
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
@@ -26,6 +35,18 @@
 
 <script>
 export default {
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route);
+    },
+    logout() {
+      this.$store.dispatch('setToken', null);
+      this.$store.dispatch('setUser', null);
+      this.$router.push({
+        name: 'root',
+      });
+    },
+  },
 };
 
 </script>
